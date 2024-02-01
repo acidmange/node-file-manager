163b076090c6d970e092556e81111e458297ac19
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-02-01 15:26:39 +0500

    refactor: change start.js structure

--- src/start/start.js
+++ src/start/start.js
@@ -1,52 +1,44 @@
-// This is program entry point
+// Program entry point
 
 import process from 'node:process';
 import { resolve } from 'node:path';
 import os from 'node:os';
+import { inputParse } from '../inputParse/inputParse.js';
 
 const cliStart = async () => {
     try {
         const userHomeDirectory = os.homedir();
         process.chdir(resolve(userHomeDirectory));
-
         const pattern = '--username=';
         const fArg = process.argv.slice(2).find((arg) => arg.startsWith(pattern));
 
         if (!fArg) {
-            throw new Error('No username provided');
+            console.log('No username provided, try again');
+            process.exit(0);
         }
 
         const userName = fArg.replace(pattern, '');
-
+        let exitRequest = false;
         console.log(`Welcome to the File Manager, ${userName}!\n`);
         console.log(`You are currently in ${process.cwd()}\n`);
 
-
-        let exitRequest = false;
-
         process.on('SIGINT', () => {
-            console.log(`\nThank you for using File Manager, ${userName}, goodbye!\n`);
+            console.log(`\n\nThank you for using File Manager, ${userName}, goodbye!`);
             process.exit(0);
         });
 
         while (!exitRequest) {
-            console.log('Enter a command (e.g., cd path_to_directory or ".exit" to exit): \n');
+            console.log('Enter a command (e.g., cd path_to_directory) or exit (command .exit / Ctrl+C combination): \n');
 
             const userInput = await new Promise((resolve) => {
                 process.stdin.once('data', (data) => resolve(data.toString().trim()));
-            });
+            });  
 
-            console.log(`\nYou are currently in ${process.cwd()}\n`);
-
-            switch (userInput) {
-                case '.exit': {
-                    exitRequest = true;
-                    console.log(`\nThank you for using File Manager, ${userName}, goodbye!`);
-                    break;
-                }
-                default: {
-                    console.log(`Invalid input\n`);
-                }
+            if (userInput === '.exit') {
+                exitRequest = true;
+                console.log(`\nThank you for using File Manager, ${userName}, goodbye!\n`);
+            } else {
+                inputParse(userInput);
             }
         }
     } catch (err) {
