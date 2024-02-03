95f27bf4ad256dad06d76c5388ffd282861ddf56
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-02-03 18:57:53 +0500

    refactor: add new prompt functions to smallfunctions.js

--- src/smallFunctions.js
+++ src/smallFunctions.js
@@ -2,4 +2,13 @@ const pwd = () => {
     console.log(`\nYou are currently in ${process.cwd()}\n`);
 };
 
-export { pwd };
+const prompt = () => {
+    console.log('Enter a command (e.g., cd "path_to_directory") or exit (command .exit / Ctrl+C combination): \n');
+};
+
+const pwdPrompt = () => {
+    pwd();
+    prompt();
+}
+
+export { pwd, prompt, pwdPrompt };
