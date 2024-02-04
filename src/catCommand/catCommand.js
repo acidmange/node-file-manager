498fa39dd867088081d5bbe0bc68cbb469229bc0
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-02-04 21:04:53 +0500

    feat: add catCommand

---
+++ src/catCommand/catCommand.js
@@ -0,0 +1,44 @@
+// show file's content in console
+// cat joe\ and\ fiby
+// cat asd asd asd
+import * as path from 'node:path';
+import { pwdPrompt, resolveInput } from '../smallFunctions.js';
+import * as fsPromises from 'fs/promises';
+import * as fs from 'node:fs';
+
+const catCommand = async (userInput, userCommand) => {
+    try {
+        const secondArg = resolveInput(userInput, userCommand);
+        const absolutePath = path.resolve(process.cwd(), secondArg);
+        const stat = await fsPromises.stat(absolutePath);
+        const isDir = stat.isDirectory();
+
+        if (isDir) {
+            console.log('\nOperation failed');
+            pwdPrompt();
+            return;
+        } else {
+            const rStream = fs.createReadStream(absolutePath);
+            const chunks = [];
+
+            rStream.on('readable', () => {
+                let chunk;
+                while (null !== (chunk = rStream.read())) {
+                    chunks.push(chunk);
+                }
+            });
+
+            rStream.on('end', () => {
+                const content = chunks.join('');
+                process.stdout.write(`\n${content}`);
+                pwdPrompt();
+            });
+        }
+
+    } catch (err) {
+        console.log('\nInvalid input');
+        pwdPrompt();
+    }
+};
+
+export { catCommand };
